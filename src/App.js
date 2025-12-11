cc0f960ff0ddd1934be263acc81d955284112b6a
Divakar Reddy <kittu.diva1999@gmail.com>
2025-12-11 19:45:57 +0530

    Initial commit - React project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Jenkins → React → Tomcat Deployment Demo</h1>
+      <h3>Deployed Successfully!</h3>
+    </div>
+  );
+}
+
+export default App;
