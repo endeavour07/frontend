9412b141c044a94e64d2070bad91afdfaf89e87b
Divakar Reddy <kittu.diva1999@gmail.com>
2025-12-15 10:06:33 +0530

    Describe my changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Test3</h1>
+      <h1>Testsing restore</h1>
       <h3>Deployed Successfully!</h3>
     </div>
   );
