364ed43320b1ac1889506b841a3bafff7f6a0aa6
Divakar Reddy <kittu.diva1999@gmail.com>
2025-12-15 10:09:19 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Test2</h1>
+      <h1>Testing restore</h1>
       <h3>Deployed Successfully!</h3>
     </div>
   );
